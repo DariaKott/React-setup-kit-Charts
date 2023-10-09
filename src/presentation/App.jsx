733f831e6301cb66216ed4fae056df633e1b061e
Daria Kot <poturayko@gmail.com>
2023-10-09 14:34:22 +0200

    Added App.jsx

---
+++ src/presentation/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Chart } from 'react-google-charts';
+
+const data = [
+  ['Confesion', 'Percent of population'],
+  ['Eastern Orthodoxy', 72],
+  ['Islamic', 19.11],
+  ['Catholics', 3.44],
+  ['Others', 4.14],
+  ['Atheist', 1.31],
+];
+
+const options = {
+  title: 'Religions in Montenegro',
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={'100%'}
+        height={'400px'}
+      />
+    </div>
+  );
+}
+
+export { App };
